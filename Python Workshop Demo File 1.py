0ba51dc034232c5dd9a3ade7aa566f3a55896d80
TheRMeister <165713760+TheRMeister12@users.noreply.github.com>
2025-09-17 10:06:31 -0500

    Add files via upload

---
+++ Python Workshop Demo File 1.py
@@ -0,0 +1,20 @@
+# Printing a message to the console
+print("Hello! Welcome to the Program!")
+
+# Obtaining user input as an integer
+num = int(input("Please enter a number: "))
+
+# Modifying the integer input (Multiply by 3)
+num = num * 3
+
+# Condition 1
+if num < 5:
+    print("Number is less than 5")
+
+#Condition 2
+elif num == 5:
+    print("Number is equal to 5")
+
+# Condition 3
+else:
+    print("Number is greater than 5")
